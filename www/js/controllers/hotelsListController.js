6f3df13fbebc5ede051002257033ea2d5f7823ac
Федченко Михаил <fedchenko@softlab.ru>
2021-12-01 10:57:37 +0300

    feat(core): Добавил фильтры для главного списка отелей.

--- www/js/controllers/hotelsListController.js
+++ www/js/controllers/hotelsListController.js
@@ -1,19 +1,28 @@
 app.controller('hotelsListController', ['$rootScope', '$scope', '$state', 'toBook', 'hotelListHttpSrvc', '$ionicSideMenuDelegate',
   function ($rootScope, $scope, $state, toBook, hotelListHttpSrvc, $ionicSideMenuDelegate) {
+
     $scope.toBook = toBook.toBook
     $scope.toBookedHotels = toBookedHotels
+    $scope.changeSelected = changeSelected
+    $scope.sortOptions = ['default', 'price ascending', 'price descending',  'rate(the best first)', 'no smoke', 'no pets']
+    $scope.changeSelected = changeSelected
+    $scope.selected = $scope.sortOptions[0]
 
+    function changeSelected(sel) {
+      $scope.selected = sel
+    }
     function toBookedHotels() {
       $state.go('bookedList')
     }
 
+    console.log($scope);
     $scope.toggleRight = function () {
       $ionicSideMenuDelegate.toggleRight();
     }
-
     hotelListHttpSrvc.getHotelsList()
       .then(data => $scope.hotelsList = data)
       .catch(err => $scope.error = err)
   }]
 )
+
 
